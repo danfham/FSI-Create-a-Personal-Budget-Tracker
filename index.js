d345638ade65a07a22f8961a71ab9b432222c8a3
danfham <dan.cunningham91@icloud.com>
2023-03-15 19:30:21 -0400

    updated index.js with contents to populate weekly exexpenses

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
